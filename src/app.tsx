313a540b55280bb40313880560ed10a1cb560cde
ryryry-3302 <rjleong2002@gmail.com>
2024-05-28 12:48:47 +0800

    feat: added template tiptap editor and some file formating

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client';
+import React from 'react';
+import  Tiptap  from './renderer/components/editor/editor';
 
 const element = document.getElementById('react-app-root');
 if (element) {
   const root = createRoot(element);
-  root.render(<h2>Hello from React!</h2>);
+  root.render(
+  <>
+    <Tiptap />
+  </>
+  );
 } else {
   console.error("There is no element in index.html with id='react-app-root'");
 }
