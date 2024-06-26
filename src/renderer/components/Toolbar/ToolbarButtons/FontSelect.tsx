9d4570eb01605c1f2531cd45d1b8bee8d64d3fd4
Zachary Kang <zacharykangjh@gmail.com>
2024-06-26 22:45:07 +0800

    fix: focus on editor after changing the font
    Calling .focus() in handleFontChange doesn't work for some reason. This issue is mentioned in the code comments.

--- src/renderer/components/Toolbar/ToolbarButtons/FontSelect.tsx
+++ src/renderer/components/Toolbar/ToolbarButtons/FontSelect.tsx
@@ -16,8 +16,14 @@ export default function FontSelect() {
 
   const handleFontChange = (newFont: string) => {
     setCurrentFont(newFont);
+    // ISSUE: The .focus() here doesn't work for some reason
+    // I think it has something to do with the default focus behavior of the shadcn <Select> component
     editor?.chain().focus().setFontFamily(newFont).run();
   };
+
+  // This .focus() call has to be here in order for the editor to be refocused after the font is changed
+  // Do not remove this unless a solution to the above issue is found
+  editor?.commands.focus();
 
   const selectItems = fontFamilies.sort().map((fontFamily) => (
     <SelectItem
@@ -30,14 +36,16 @@ export default function FontSelect() {
   ));
 
   return (
-    <Select onValueChange={handleFontChange} defaultValue='Inter Variable'>
-      <SelectTrigger
-        className='m-px h-9 w-40 border-none bg-slate-200 ring-inset focus:ring-1 focus:ring-slate-700 focus:ring-offset-0'
-        style={{ fontFamily: currentFont }}
-      >
-        <SelectValue />
-      </SelectTrigger>
-      <SelectContent>{selectItems}</SelectContent>
-    </Select>
+    <>
+      <Select onValueChange={handleFontChange} defaultValue='Inter Variable'>
+        <SelectTrigger
+          className='m-px h-9 w-40 border-none bg-slate-200 ring-inset focus:ring-1 focus:ring-slate-700 focus:ring-offset-0'
+          style={{ fontFamily: currentFont }}
+        >
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>{selectItems}</SelectContent>
+      </Select>
+    </>
   );
 }
