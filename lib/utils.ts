00af52aa03ff5f465028a1bdafc3128841f8ebe4
Zachary Kang <zacharykangjh@gmail.com>
2024-06-28 11:06:46 +0800

    refactor: move getProseMirror utility function to utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,14 @@ export function cn(...inputs: ClassValue[]) {
 export function stripTrailingVariable(str: string) {
   return str.replace(/ Variable$/, '');
 }
+
+export function getProseMirror() {
+  const elements = document.getElementsByClassName('ProseMirror');
+  if (elements.length > 1) {
+    console.error(
+      "There is more than one HTML element with the class 'ProseMirror'!"
+    );
+  }
+  const prosemirror = elements[0] as HTMLDivElement;
+  return prosemirror;
+}
